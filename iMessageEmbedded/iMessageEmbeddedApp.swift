014359712114a6f13c0945a1e75425df99182c4d
Justin Bush <revblaze@users.noreply.github.com>
2021-02-15 13:30:59 -0500

    Create SwiftUI project

---
+++ iMessageEmbedded/iMessageEmbeddedApp.swift
@@ -0,0 +1,17 @@
+//
+//  iMessageEmbeddedApp.swift
+//  iMessageEmbedded
+//
+//  Created by Justin Bush on 2021-02-15.
+//
+
+import SwiftUI
+
+@main
+struct iMessageEmbeddedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
